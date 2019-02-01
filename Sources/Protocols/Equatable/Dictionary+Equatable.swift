deb71b73eaae819cd636ae832528e5df5a595117
iDonJose <donor.develop@gmail.com>
2019-02-02 00:11:49 +0100

    Improved Dictionary+Equatable

--- Sources/Protocols/Equatable/Dictionary+Equatable.swift
+++ Sources/Protocols/Equatable/Dictionary+Equatable.swift
@@ -12,3 +12,25 @@ import Foundation
 public func == <Key: Hashable, Value>(lhs: [Key: Value], rhs: [Key: Value] ) -> Bool {
 	return NSDictionary(dictionary: lhs).isEqual(rhs)
 }
+
+public func != <Key: Hashable, Value>(lhs: [Key: Value], rhs: [Key: Value] ) -> Bool {
+	return !(lhs == rhs)
+}
+
+
+public func == <Key: Hashable, Value>(lhs: [Key: Value]?, rhs: [Key: Value]?) -> Bool {
+	switch (lhs, rhs) {
+	case (nil, nil):
+		return true
+	case (nil, _),
+		 (_, nil):
+		return false
+	default:
+		return lhs! == rhs!
+	}
+}
+
+public func != <Key: Hashable, Value>(lhs: [Key: Value]?, rhs: [Key: Value]?) -> Bool {
+	return !(lhs == rhs)
+}
+
